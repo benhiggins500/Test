65bf65ac8240589a3a0fd42c15ecae23f71f76cf
Ben Higgins <ben@benhiggins.com>
2025-12-16 17:05:18 +0000

    a

---
+++ src/TestSolution/EdgeDriverTest1/AppConfigTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdgeDriverTest1
+{
+
+    [TestClass]
+    public class AppConfigTest
+    {
+        [TestMethod]
+        public void TestAppConfigSettingDefaultFile()
+        {
+            // Replace with your own test logic to verify app configuration settings
+            string expectedValue = "RepoValue";
+            string actualValue = ConfigurationManager.AppSettings["Test"];
+            Assert.AreEqual(expectedValue, actualValue, "The app configuration setting does not match the expected value.");
+        }
+
+        [TestMethod]
+        public void TestAppConfigSettingWrittenFile()
+        {
+            // Replace with your own test logic to verify app configuration settings
+            string expectedValue = "GitHubValue";
+            string actualValue = ConfigurationManager.AppSettings["Test"];
+            Assert.AreEqual(expectedValue, actualValue, "The app configuration setting does not match the expected value.");
+        }
+    }
+}
